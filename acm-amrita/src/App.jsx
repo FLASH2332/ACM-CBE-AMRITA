d449834b4505c2dcc3d92cc51da94059a5440db5
FLASH2332 <jayaveeran2005@gmail.com>
2025-09-30 21:01:53 +0530

    Initial setup for React+Tailwind

---
+++ acm-amrita/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-slate-900 min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold underline text-cyan-400">
+        ACM Club Website Setup Complete!
+      </h1>
+    </div>
+  )
+}
+
+export default App
+
